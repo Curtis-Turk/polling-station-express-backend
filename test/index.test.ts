2bf89aa34c223a7a176de92324ce06d8511d812c
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-15 15:20:38 +0000

    Added a timeout for the axios response

--- test/index.test.ts
+++ test/index.test.ts
@@ -119,6 +119,26 @@ describe("app", () => {
     });
   });
 
+  it("returns an error message if axios hangs", async () => {
+    const postcodeRequest = { postcode: "TN4TWH" };
+    mockedAxiosGet.mockResolvedValue(new AxiosError());
+    const result = await supertest(app)
+      .post("/postcode")
+      .set("origin", process.env.FRONT_END_DOMAIN)
+      .send(postcodeRequest);
+    expect(mockedAxiosGet).toHaveBeenCalledWith(
+      `https://api.electoralcommission.org.uk/api/v1/postcode/TN4TWH?token=${process.env.EC_API_KEY}`
+    );
+    expect(result.status).toBe(400);
+    expect(result.body).toEqual({
+      errorMessage: "Connection issue whilst verifying postcode",
+      pollingStationFound: false,
+      pollingStations: [],
+    });
+  });
+
+  // Test if axios times out
+
   it("returns a 400 status with incorrect origin", async () => {
     const postcodeRequest = { postcode: "TN4TWH" };
     await supertest(app).post("/postcode").send(postcodeRequest).expect(400);
